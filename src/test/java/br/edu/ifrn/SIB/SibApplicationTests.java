d3ff1e6c0fabe32a26c22896a499b0301090e487
Marcelo Júnior <rokermarcelo@gmail.com>
2023-10-04 13:40:19 -0300

    Carregando projeto do Framework Spring

---
+++ src/test/java/br/edu/ifrn/SIB/SibApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.SIB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
