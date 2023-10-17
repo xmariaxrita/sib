d12ef5297dc8346bf0ba0c5d325cf070d959259a
Willana <wlaninha35@gmail.com>
2023-10-17 16:05:36 -0300

    Criação da tela inicial do catálogo

---
+++ src/main/java/br/ifrn/pi/sib/models/Catalogue.java
@@ -0,0 +1,90 @@
+package br.ifrn.pi.sib.models;
+
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "acervo")
+public class Catalogue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name="autor")
+    private String autor;
+
+    @Column(name="classificacao_indicativa")
+    private int classificacao_indicativa;
+
+    @Column(name="lancamento")
+    private Date lancamento;
+
+    @Column(name="titulo")
+    private String titulo;
+
+    @Column(name="codigo")
+    private int codigo;
+
+    public Catalogue(int id, String autor, int classificacao_indicativa, Date lancamento, String titulo, int codigo) {
+        this.id = id;
+        this.autor = autor;
+        this.classificacao_indicativa = classificacao_indicativa;
+        this.lancamento = lancamento;
+        this.titulo = titulo;
+        this.codigo = codigo;
+    }
+
+    public Catalogue() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getClassificacao_indicativa() {
+        return classificacao_indicativa;
+    }
+
+    public void setClassificacao_indicativa(int classificacao_indicativa) {
+        this.classificacao_indicativa = classificacao_indicativa;
+    }
+
+    public Date getLancamento() {
+        return lancamento;
+    }
+
+    public void setLancamento(Date lancamento) {
+        this.lancamento = lancamento;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+}
