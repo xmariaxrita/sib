c9b557b824c8b0ea2a4d08bd83c5209dc009ec15
ana0beatrix <beatriz.farias@escolar.ifrn.edu.br>
2023-10-18 14:34:26 -0300

    Função de excluir usuário

--- src/main/java/br/ifrn/pi/sib/controllers/UserController.java
+++ src/main/java/br/ifrn/pi/sib/controllers/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -20,5 +22,17 @@ public class UserController {
         List<User> users = (List<User>) repo.findAll();
         model.addAttribute("users", users);
         return "users/index";
+    }
+
+    @PostMapping("/users/criar")
+    public String criar(User user){
+        repo.save(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping ("/users/{id}/excluir")
+    public String excluir(@PathVariable int id){
+        repo.deleteById(id);
+        return "redirect:/users";
     }
 }
