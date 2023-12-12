e40a0e4ee176b686bbc44e9a6e4e70717c8ee42e
Maria <xmariaxrita4@gmail.com>
2023-12-12 17:50:21 -0300

    Adição das configurações da tela de login

--- src/main/java/br/ifrn/pi/sib/repositorio/UserRepo.java
+++ src/main/java/br/ifrn/pi/sib/repositorio/UserRepo.java
@@ -1,7 +1,13 @@
 package br.ifrn.pi.sib.repositorio;
 
 import br.ifrn.pi.sib.models.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepo extends CrudRepository<User, Integer> {
+    @Query(value = "SELECT CASE WHEN COUNT(1)> 0 THEN 'true' ELSE 'false' END from users where id = :id", nativeQuery = true)
+    public boolean exist(int id);
+
+    @Query(value = "SELECT * FROM users WHERE email = :email AND senha = :senha", nativeQuery = true)
+    public User Login(String email, String senha);
 }
