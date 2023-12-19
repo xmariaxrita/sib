299570564d7d223d73a64390da0976e18a7807d9
Palmiere <palmiere.santana@escolar.ifrn.edu.br>
2023-12-19 16:08:14 -0300

    Upload de imagem catalogo

--- src/main/java/br/ifrn/pi/sib/controllers/CatologueController.java
+++ src/main/java/br/ifrn/pi/sib/controllers/CatologueController.java
@@ -8,12 +8,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Controller
 public class CatologueController {
 
+    public static String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/src/main/resources/static/uploads/";
     @Autowired
     private CatalogueRepo repo;
 
@@ -47,7 +54,11 @@ public class CatologueController {
     }
 
     @PostMapping("/catalogues/save")
-    public String novo(Catalogue catalogue){
+    public String novo(Catalogue catalogue, @RequestParam("imgCapa") MultipartFile file) throws IOException {
+        StringBuilder fileNames = new StringBuilder();
+        Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, file.getOriginalFilename());
+        fileNames.append(file.getOriginalFilename());
+        Files.write(fileNameAndPath, file.getBytes());
         repo.save(catalogue);
         return "redirect:/catalogues";
 
