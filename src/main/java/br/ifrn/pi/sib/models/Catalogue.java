35ddc3928ec82d2a4ca89dd697a89d76a447c16a
Eloíza Brilhante <e.brilhante@escolar.ifrn.edu.br>
2023-12-26 15:37:51 -0300

    correção do redirecionamento da página com layout bonito

--- src/main/java/br/ifrn/pi/sib/models/Catalogue.java
+++ src/main/java/br/ifrn/pi/sib/models/Catalogue.java
@@ -9,32 +9,72 @@ public class Catalogue {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
+    @Column(name = "titulo")
+    private String titulo;
 
     @Column(name = "autor")
     private String autor;
 
-    @Column(name = "classificacao_indicativa")
-    private int classificacao_indicativa;
+    @Column(name = "colecao")
+    private String colecao;
+
+    @Column(name = "volume")
+    private int volume;
+
+    @Column(name = "editora")
+    private String editora;
 
     @Column(name = "lancamento")
     private int lancamento;
 
-    @Column(name = "titulo")
-    private String titulo;
+    @Column(name = "edicao")
+    private int edicao;
 
-    @Column(name = "codigo")
-    private int codigo;
+    @Column(name = "anoEdicao")
+    private int anoEdicao;
 
-    public Catalogue(int id, String autor, int classificacao_indicativa, int lancamento, String titulo, int codigo) {
-        this.id = id;
-        this.autor = autor;
-        this.classificacao_indicativa = classificacao_indicativa;
-        this.lancamento = lancamento;
-        this.titulo = titulo;
-        this.codigo = codigo;
-    }
+    @Column(name = "txtNumPaginas")
+    private int txtNumPaginas;
+
+    @Column(name = "classificacao_indicativa")
+    private int classificacao_indicativa;
+
+    @Column(name = "genero")
+    private String genero;
+
+    @Column(name = "isbn10")
+    private String isbn10;
+
+    @Column(name = "isbn13")
+    private String isbn13;
+
+    @Column(name = "copias")
+    private int copias;
+
+    @Column(name = "sinopse")
+    private String sinopse;
+
 
     public Catalogue() {
+    }
+
+    public Catalogue(int id, String titulo, String autor, String colecao, int volume, String editora, int lancamento, int edicao, int anoEdicao, int txtNumPaginas, int classificacao_indicativa, String genero, String isbn10, String isbn13, int copias, String sinopse) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.colecao = colecao;
+        this.volume = volume;
+        this.editora = editora;
+        this.lancamento = lancamento;
+        this.edicao = edicao;
+        this.anoEdicao = anoEdicao;
+        this.txtNumPaginas = txtNumPaginas;
+        this.classificacao_indicativa = classificacao_indicativa;
+        this.genero = genero;
+        this.isbn10 = isbn10;
+        this.isbn13 = isbn13;
+        this.copias = copias;
+        this.sinopse = sinopse;
     }
 
     public int getId() {
@@ -45,6 +85,14 @@ public class Catalogue {
         this.id = id;
     }
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
     public String getAutor() {
         return autor;
     }
@@ -53,12 +101,28 @@ public class Catalogue {
         this.autor = autor;
     }
 
-    public int getClassificacao_indicativa() {
-        return classificacao_indicativa;
+    public String getColecao() {
+        return colecao;
     }
 
-    public void setClassificacao_indicativa(int classificacao_indicativa) {
-        this.classificacao_indicativa = classificacao_indicativa;
+    public void setColecao(String colecao) {
+        this.colecao = colecao;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
     }
 
     public int getLancamento() {
@@ -69,19 +133,77 @@ public class Catalogue {
         this.lancamento = lancamento;
     }
 
-    public String getTitulo() {
-        return titulo;
+    public int getEdicao() {
+        return edicao;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
+    public void setEdicao(int edicao) {
+        this.edicao = edicao;
     }
 
-    public int getCodigo() {
-        return codigo;
+    public int getAnoEdicao() {
+        return anoEdicao;
     }
 
-    public void setCodigo(int codigo) {
-        this.codigo = codigo;
+    public void setAnoEdicao(int anoEdicao) {
+        this.anoEdicao = anoEdicao;
     }
+
+    public int getTxtNumPaginas() {
+        return txtNumPaginas;
+    }
+
+    public void setTxtNumPaginas(int txtNumPaginas) {
+        this.txtNumPaginas = txtNumPaginas;
+    }
+
+    public int getClassificacao_indicativa() {
+        return classificacao_indicativa;
+    }
+
+    public void setClassificacao_indicativa(int classificacao_indicativa) {
+        this.classificacao_indicativa = classificacao_indicativa;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getIsbn10() {
+        return isbn10;
+    }
+
+    public void setIsbn10(String isbn10) {
+        this.isbn10 = isbn10;
+    }
+
+    public String getIsbn13() {
+        return isbn13;
+    }
+
+    public void setIsbn13(String isbn13) {
+        this.isbn13 = isbn13;
+    }
+
+    public int getCopias() {
+        return copias;
+    }
+
+    public void setCopias(int copias) {
+        this.copias = copias;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+
 }
